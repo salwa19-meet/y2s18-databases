eda36171856162844a0bd0ec8f3df043ccf19c49
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 11:59:43 +0300

    aaa

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,53 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name, year, country):
+    article_opject =    Knowledge(
+        name=name,
+        year=year,
+        country=country)
+    session.add(article_opject)
+    session.commit()
+
+
+    
 
 def query_all_articles():
-	pass
+    knowledge = session.query(
+       Knowledge).all()
+    return knowledge
+
+ 
+    
 
 def query_article_by_topic():
-	pass
+
+    knowledge = session.query(knowledge).filter_by(topic = topic).first()
+    return knowledge
+    
 
 def delete_article_by_topic():
-	pass
+    
+    knowledge = session.query(knowledge).filter_by(topic = topic).delete()
+    session.commit()
+    
 
 def delete_all_articles():
-	pass
+    knowledge = session.query(knowledge).all().delete()
+    session.commit()
 
-def edit_article_rating():
-	pass
+
+def edit_article_rating(topic, rating):
+    article_opject = session.query(knowledge).filter_by(topic = topic).first()
+    article_opject_rating = new_rating
+    session.commit()
+        
+
+
+add_article("pizza", 1698 , "itali") 
+print(query_all_articles())  
+
+
+
+
+
